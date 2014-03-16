0cee7151411fb3cae5961c5096b10dfe8d5bd94f
WarrenLearn <warren.learnanything@gmail.com>
2014-03-16 16:42:15 -0500

    Interpolated doubles and chars.

--- lesson2/variables.c
+++ lesson2/variables.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 int main()
 {
-	int age = 23;
-	printf("I am %d years old.\n",age);
+	int age = 5;
+	printf("Age: %d\n", age);
+	/* %d is the integer spaceholer */
+
+	double price = 20.50;
+	printf("The shirt cost %lf\n", price);
+	/* %lf is the spaceholder for doubles */
+
+	char first_initial = 'W';
+	printf("My name starts with a %c\n", first_initial);
+
+	/* char's have single qoutes */
+
+	
 }
